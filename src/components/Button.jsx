75e6326da0ec4942f4ed284b5dd5c8e178b5369d
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-03-08 14:58:41 +0700

    Revert "Import assets to index.html"
    This reverts commit 98d0c436df70c1862158f3b2fcdfab4c8b01e921.

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = (props) => {
-  return <button type="button" class="btn btn-lg bg-gradient-info btn-lg w-100 mt-4 mb-0">{props.label}</button>
-}
-
-export default Button;
--- src/components/Image.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Image = (props) => {
-  return <img src={props.path} alt={props.alt} class={props.className} />
-}
-
-export default Image;
