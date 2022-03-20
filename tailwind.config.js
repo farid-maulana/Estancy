dfddc2873a21a3e4af8b556c8b22b6cf3c719c56
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-03-20 19:07:00 +0700

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#6078F8',
+      'white': '#FFFFFF',
+      'secondary': '#E35500',
+      'green': '#00A171',
+      'black': '#000000',
+      'primary-100': '#E9EFF6',
+    }
   },
   plugins: [],
 }
