6d9e81c8106959d53bb8a6e73634b064c5d758cd
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-03-20 22:26:54 +0700

    Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import InputField from './components/InputField';
-import Button from './components/Button';
+import Login from './pages/auth/Login';
 
 function App() {
   return (
-    <div className='container mx-auto'>
-      <InputField type={'email'} id={'email'} name={'email'} placeholder={'Enter your email'}/>
-      <InputField type={'password'} id={'password'} name={'password'} placeholder={'Enter your password'}/>
-      <Button value={'Sign In'}/>
-    </div>
+    <Login/>
   );
 }
 
