279f6e499e53454c1ad99aa0c8a4ef9391486291
Dafa Bintang Athaillah <Dafabintang76@gmail.com>
2022-03-22 18:21:14 +0800

    Create reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import ResetPassword2 from './pages/auth/ResetPassword2'
 
 function App() {
   return (
-    <Register/>
+    <ResetPassword2/>
   );
 }
 
---
+++ src/pages/auth/ResetPassword2.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import InputField from '../../components/InputField'
+import Button from '../../components/Button'
+import IconButton from '../../components/IconButton'
+
+const ResetPassword2 = () => {
+    return (
+        <div className='container mx-auto'>
+            <div className='flex justify-start items-center h-screen'>
+                <div className='w-3/5'>
+                    <img srcSet='img/Illustration.png' className='w-5/6 mx-auto mb-5' />
+                    <h1 className='text-6xl font-semibold text-center mb-5'>Estancy</h1>
+                    <p className='text-base text-center'>The shortest distance between paradise and the place you call home</p>
+                </div>
+                <div className='w-2/5 my-auto pr-32 pl-8'>
+                    <h4 className='text-2xl font-semibold mb-3'>Reset Password</h4>
+                    <p className='text-base mb-3 font-normal text-base'>Create a new password</p>
+                    <InputField type={'password'} id={'password'} name={'password'} placeholder={'Password'} />
+                    <InputField type={'password'} id={'password'} name={'password'} placeholder={'Confirm Password'} />
+                    <Button value={'Reset Password'} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResetPassword2
