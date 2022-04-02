a756c20ee92927bb59f88c58c0560ae66b4d665b
Dafa Bintang Athaillah <Dafabintang76@gmail.com>
2022-04-02 09:43:37 +0800

    Proggress VillaCard

--- src/components/VillaCard.jsx
+++ src/components/VillaCard.jsx
@@ -1,52 +1,24 @@
 import React from 'react'
 
-const VillaCard = ({img, name, reviews, address, price}) => {
+const VillaCard = () => {
   return (
     <div class="p-10">
       <div class="w-80 h-96 left-0.5 top-2 shadow-md rounded-lg bg-white flex-none order-none grow-0 my-0 mx-7">
-        <img srcSet={img} className='w-80 h-48 left-0 top-0 rounded-lg' />
+        <img srcSet='img/icons/room.png' className='w-80 h-48 left-0 top-0 rounded-lg' />
         <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">
-            <p className='absolute w-48 h-11 top-60 font-sans not-italic '>{name}</p>
-          </div>
-          <div className='flex'>
-            <div className='flex flex w-20 mt-7 h-4 left-3 top-72'>
-              <ul class="flex ">
-                <li>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
-                  </svg>
-                </li>
-                <li>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
-                  </svg>
-                </li>
-                <li>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
-                  </svg>
-                </li>
-                <li>
-                  <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                  </svg>
-                </li>
-                <li>
-                  <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                  </svg>
-                </li>
-              </ul>
+          <div className='flex justify-between'>
+            <div class=" mb-2">
+              <p className='font-bold text-xl w-48 h-11 top-60 font-sans not-italic '>Villa Pertama</p>
             </div>
-            <div className='flex mt-7'>
-              <p className='ml-6 font-sans non-italic font-normal text-xs leading-4 text-center'>
-              {reviews}
-              </p>
+            <div className='flex mb-2'>
+              <div className='text-sm mr-3'>
+                <p>love</p>
+              </div>
+              <div className='text-sm'>
+                <p>share</p>
+              </div>
             </div>
           </div>
-          <p className='mt-3 font-sans non-italic font-normal mb-7'>{address}</p>
-          <p className='mt-3 font-bold text-xl'>{price}</p>
         </div>
         <div class="px-6 pt-4 pb-2">
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"></span>
