ef3a8a0b81c04ccbab34c6db1ee18ff991ef4948
nsenja <nabilasw34@gmail.com>
2022-04-04 08:02:30 +0700

    Create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-evenly flex-wrap h-32'>
             <h1 className='not-italic text-4xl font-semibold w-32 h-11 left-32 top-10 absolute'>Estancy</h1>
-            <div className='flex w-3/5'></div>
-            <Link to={'/'} className='mx-2.5 w-10 h-6 font-normal text-base'>Home</Link>
-            <a href="#" className='flex-w-12 h-6 font-normal text-base'>Property</a>
-            <Link to={'/about-us'} className='flex-w-10 h-6 font-normal text-base'>About Us</Link>
-            <a href="#" className='flex-w-10 h-6 font-normal text-base'>Contact Us</a>
-            <div className='mr-32 box-border drop-shadow-lg rounded-lg border border-primary'>
+            <div className='flex w-1/5'></div>
+            <Link to={'/'} className='ml-14 h-6 font-normal text-base'>Home</Link>
+            <a href="#" className='h-6 font-normal text-base'>Property</a>
+            <Link to={'/about-us'} className='h-6 font-normal text-base'>About Us</Link>
+            <a href="#" className='mr-28 h-6 font-normal text-base '>Contact Us</a>
+            <div className='mr-32 drop-shadow-lg rounded-lg bg-primary'>
                 <Link to={'/login'}>
-                    <button className='left-3.5 top-2.5 grow font-medium px-3.5 py-1 text-base
-                text-center text-primary'>Login</button>
+                    <button className='left-3.5 top-2.5 grow font-medium px-4 py-2.5 text-base text-lg
+                text-center text-white'>Get Started</button>
                 </Link>
             </div>
         </div>
